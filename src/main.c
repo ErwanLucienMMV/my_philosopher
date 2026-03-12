3536701768fb8f9e612aface7e1e4b1865c04e89
Erwan Maigne <emaigne@f4r7s4.clusters.42paris.fr>
2026-03-12 01:13:54 +0100

    philos init done

--- src/main.c
+++ src/main.c
@@ -6,18 +6,67 @@
 /*   By: emaigne <emaigne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 14:15:13 by emaigne           #+#    #+#             */
-/*   Updated: 2026/03/09 14:42:54 by emaigne          ###   ########.fr       */
+/*   Updated: 2026/03/12 01:13:32 by emaigne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
+#include "philo.h"
+
+static void	assign_forks(t_table *table, int i)
+{
+	int	n;
+
+	n = table->args.number_of_philo;
+	table->philos[i].left_fork = &table->forks[i];
+	table->philos[i].right_fork = &table->forks[(i + 1) % n];
+}
+
+static int	init_philo(t_table *table, int i)
+{
+	t_philo	*philo;
+
+	philo = &table->philos[i];
+	philo->id = i + 1;
+	philo->last_meal = 0;
+	philo->meals_eaten = 0;
+	philo->table = table;
+	if (pthread_mutex_init(&philo->meal_mutex, NULL) != 0)
+		return (1);
+	assign_forks(table, i);
+	return (0);
+}
+
+t_philo	*initialize_philo(t_table *table)
+{
+	int	i;
+	int	n;
+
+	n = table->args.number_of_philo;
+	table->philos = malloc(sizeof(t_philo) * n);
+	if (!table->philos)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		if (init_philo(table, i))
+			return (NULL);
+		i++;
+	}
+	return (table->philos);
+}
+
 int	main(int argc, char **argv)
 {
 	t_args	args;
+	t_table	table;
 
 	if (parse_args(argc, argv, &args))
 		return (1);
-	print_params(&args);
+	table.args = args;
+	table.philos = initialize_philo(&table);
+	if (!table.philos)
+		return (1);
 	return (0);
 }
